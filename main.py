54042c8e53eb82b30d3eda49169522bc5f8deaf1
mcolalongo <mattiacolalongo82@gmail.com>
2025-04-02 13:54:55 +0200

    viz update

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-# file = fileIO(r"C:\Users\Mattia Colalongo\OneDrive - Novac SRL\Sharezone\Novac Condivisa\R&D materials\Cartelle_Personali\Mattia_Colalongo\LFP_Gr_ST\graphite_capacity")
-# file.conversion()
+file = fileIO(r"C:\Users\Mattia Colalongo\OneDrive - Novac SRL\Sharezone\Novac Condivisa\R&D materials\Cartelle_Personali\Mattia_Colalongo\LFP_Gr_ST\graphite_capacity")
+file.conversion()
 # file.extract_charge()
 # file.extract_discharge()
 
 file = fileIO(r"C:\Users\Mattia Colalongo\OneDrive - Novac SRL\Sharezone\Novac Condivisa\R&D materials\Cartelle_Personali\Mattia_Colalongo\LFP_Gr_ST\graphite_capacity\csv")
 file.extract_charge()
-file.extract_discharge(()
+file.extract_discharge()
