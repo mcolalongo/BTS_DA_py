365003191c217fa9c0eb1da5abae33c0b3502c9c
mcolalongo <mattiacolalongo82@gmail.com>
2025-04-02 09:44:03 +0200

    added viz

--- fio.py
+++ fio.py
@@ -1,5 +1,6 @@
 import os
-
+import pandas as pd
+import numpy as np
 
 class fileIO:
     """
@@ -15,3 +16,51 @@ class fileIO:
                 if (".xlsx" in i) and (j in i):
                     self.list.append(i)
             self.dict[f"{j.replace("-","")}"] = self.list
+
+
+    def conversion(self):
+        self.create_dir("csv")
+        for key,values in self.dict.items():
+            for i in values:
+                self.df = pd.read_excel(os.path.join(self.path, f"{i}"), sheet_name=3, engine='calamine')
+                self.df.to_csv(os.path.join(self.path,f"csv/{i}.csv"), sep=',', index=False)
+                print(f"File {i}.csv created in csv folder")
+
+    def create_dir(self, folpath):
+        self.folpath = folpath
+        if os.path.isdir(os.path.join(self.path,self.folpath)):
+            print("The folder Exists")
+        else:
+            print("The folder Doesn't exists, creating...")
+            os.mkdir(os.path.join(self.path,self.folpath))
+
+
+    def extract_charge(self):
+        self.create_dir(r"charge")    
+        for key,values in self.dict.items():
+            for i in values:
+                self.df = pd.read_csv(os.path.join(self.path, f"{i}"))
+                self.df_filter = self.df.loc[self.df["State"] == "CC Chg"]["Steps"].to_numpy()
+                for n in np.unique(self.df_filter):
+                    self.filtered = self.df.loc[(self.df["State"] == "CC Chg") & (self.df["Steps"] == n)]
+                    self.filtered.to_csv(os.path.join(self.path,f"charge\\{i}_charge_step_{n}.csv"))
+                    
+                
+    def extract_discharge(self):
+        self.create_dir(r"discharge")    
+        for key,values in self.dict.items():
+            for i in values:
+                self.df = pd.read_csv(os.path.join(self.path, f"{i}"))
+                self.df_filter = self.df.loc[self.df["State"] == "CC DChg"]["Steps"].to_numpy()
+                for n in np.unique(self.df_filter):
+                    self.filtered = self.df.loc[(self.df["State"] == "CC DChg") & (self.df["Steps"] == n)]
+                    self.filtered.to_csv(os.path.join(self.path,f"discharge\\{i}_discharge_step_{n}.csv"))
+                    
+                
+
+    # def extract_discharge(self):        
+    #     for key,values in self.dict.items():
+    #         for i in values:
+    #             self.df = pd.read_excel(os.path.join(self.path, f"{i}"), sheet_name=3, engine='calamine')
+    #             # self.df_filter = self.df.loc[self.df["State"] == "CC DChg"]["Steps"].to_numpy()
+    #             # print(np.unique(self.df_filter))
--- main.py
+++ main.py
@@ -2,18 +2,13 @@ from fio import fileIO
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 
-file = fileIO(r"C:\Users\Mattia Colalongo\OneDrive - Novac SRL\Sharezone\Novac Condivisa\R&D materials\Cartelle_Personali\Mattia_Colalongo\LFP_Gr_ST\graphite_capacity")
-print(file.dict)
+# file = fileIO(r"C:\Users\Mattia Colalongo\OneDrive - Novac SRL\Sharezone\Novac Condivisa\R&D materials\Cartelle_Personali\Mattia_Colalongo\LFP_Gr_ST\graphite_capacity")
+# file.conversion()
+# file.extract_charge()
+# file.extract_discharge()
 
-fig, ax = plt.subplots()
-for key,values in file.dict.items():
-    for i in values:
-        df = pd.read_excel(os.path.join(file.path, f"{i}"), sheet_name=3, engine='calamine')
-        df_filter = df.loc[df["State"] == "CC Chg"]
-        ax.plot(df_filter["Capacity(Ah)"],df_filter["Voltage(V)"])
-
-plt.savefig(os.path.join(file.path,"s.png"), format='png', dpi=500)
-
-        
-        
+file = fileIO(r"C:\Users\Mattia Colalongo\OneDrive - Novac SRL\Sharezone\Novac Condivisa\R&D materials\Cartelle_Personali\Mattia_Colalongo\LFP_Gr_ST\graphite_capacity\csv")
+file.extract_charge()
+file.extract_discharge(()
